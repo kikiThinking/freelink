7f590e87077a08e744ea17bcd32e03e8fb21360b
simpleBoyLazlo <y1936839825@outlook.com>
2024-09-27 16:34:22 +0800

    first commit

---
+++ lib/pages/login.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _Login();
+}
+
+class _Login extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+
+}
---
+++ lib/route/routes.dart
@@ -0,0 +1,7 @@
+import 'package:freelink/pages/login.dart';
+import 'package:flutter/material.dart';
+
+
+final Map<String, WidgetBuilder> Routes = {
+  '/Login': (context) => const Login(),
+};
